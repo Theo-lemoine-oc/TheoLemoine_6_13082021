484125c5363ccc8822f236da1f1a1106d1239195
Théo <propjhh>
2021-08-29 19:38:43 +0200

    verifyPassword and add security

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 
+const helmet = require('helmet');
+const nocache = require('nocache');
+
 const saucesRoutes = require('./routes/sauces');
 const userRoutes = require('./routes/user');
+
+// utilisation du module 'dotenv' pour masquer les informations de connexion à la base de données à l'aide de variables d'environnement
+require('dotenv').config();
 
 //Connection à la base de données
 mongoose.connect('mongodb+srv://proph:jorlobcr121233@cluster0.2fidz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
@@ -25,8 +31,16 @@ app.use((req, res, next) => {
     next();
 });
 
+
 //Remplacer le body-parser : transforme les données arrivant à la requête POST en un objet JSON facilemnet exploitable
 app.use(express.json());
+
+// Sécuriser Express en définissant divers en-têtes HTTP - https://www.npmjs.com/package/helmet#how-it-works
+// On utilise helmet pour plusieurs raisons notamment la mise en place du X-XSS-Protection afin d'activer le filtre de script intersites(XSS) dans les navigateurs web
+app.use(helmet());
+
+//Désactive la mise en cache du navigateur
+app.use(nocache());
 
 //Gérer les images
 app.use('/images', express.static(path.join(__dirname, 'images')));
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,11 +1,21 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
+const sanitizerPlugin = require('mongoose-sanitizer-plugin');
 
 const userSchema = mongoose.Schema({
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/, "Veuillez entrer une adresse email correcte"]
+    },
+    password: {
+        type: String,
+        required: [true, "Veuillez choisir un mot de passe"]
+    }
 });
 
 userSchema.plugin(uniqueValidator);
+userSchema.plugin(sanitizerPlugin);
 
 module.exports = mongoose.model('User', userSchema);
